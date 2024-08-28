0d431b01048e7130acaaae42eb88f559a9411f88
Shahbaaz Buksh <shahbaazbuksh@yahoo.com>
2024-08-29 01:13:49 +0530

    Completed: 120

--- src/index.js
+++ src/index.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+// import "./index.css";
+// import App from "./App";
+import StarRating from "./StarRating";
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p>The movie was rating {movieRating} stars.</p>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <>
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating
+      defaultRating={3}
+      maxRating={5}
+      size={24}
+      color="red"
+      className="test"
+    />
+    <Test />
+  </>
+);
